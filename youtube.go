1b6579ca5fbcf5bdde9c84a2886844372f8d7883
Serhii Podhornyi <serhii.podhornyi@ottonova.de>
2023-08-18 10:50:31 +0100

    convert video to audio

--- youtube.go
+++ youtube.go
@@ -8,17 +8,22 @@ import (
 	"github.com/kkdai/youtube/v2/downloader"
 )
 
-func YoutubeDL(ytlink string) error {
+func YoutubeDL(ytID string) (string, error) {
 	client := youtube.Client{
 		Debug: true,
 	}
 	ctx := context.Background()
 
-	video, err := client.GetVideoContext(ctx, ytlink)
+	video, err := client.GetVideoContext(ctx, ytID)
 	if err != nil {
-		return fmt.Errorf("getting video: %w", err)
+		return "", fmt.Errorf("getting video: %w", err)
 	}
 	d := downloader.Downloader{Client: client, OutputDir: "./"}
+	outputfile := ytID + ".mp4"
+	err = d.DownloadComposite(ctx, outputfile, video, "hd1080", "mp4")
+	if err != nil {
+		return "", fmt.Errorf("downloading video: %w", err)
+	}
 
-	return d.DownloadComposite(ctx, "", video, "hd1080", "mp4")
+	return ytID, nil
 }
