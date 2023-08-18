fdf7fc119fb6fe7e264b48fbddb62553099435f7
Serhii Podhornyi <serhii.podhornyi@ottonova.de>
2023-08-18 12:10:55 +0100

    delete obsolete scripts

--- buildx.sh
+++
@@ -1,1 +0,0 @@
-docker buildx build --platform linux/amd64 -t ytt-amd64 --load -f Dockerfile .
--- runx.sh
+++
@@ -1,1 +0,0 @@
-docker run --platform linux/amd64 --network surrealdb-network -v "$(pwd)":/data -e IP_ADDRESS=10.100.95.88 -it --rm ytt-amd64 get "JzPfMbG1vrE" 
